8852d0de03adda7cd4476ff4ba1f198fb81a0ac8
shmoney2 <geddsahil@gmail.com>
2026-09-06 22:34:23 -0400

    initial decorator

--- strategy/Behavior.java
+++ strategy/Behavior.java
@@ -3,3 +3,4 @@ package strategy;
 public interface Behavior {
     public String play();
 }
+
--- strategy/team.java
+++ strategy/team.java
@@ -4,36 +4,40 @@ import java.util.ArrayList;
 
 public class Team {
     private String teamName;
-	private ArrayList<Player> players;
+    private ArrayList<Player> players;
 
-public Team(String teamName) {
-		this.teamName = teamName;
-		this.players = new ArrayList<Player>();
-	}
+    public Team(String teamName) {
+        this.teamName = teamName;
+        players = new ArrayList<>();
+    }
 
-public void addTeamMember(String firstName, String lastName, PlayerType playerType) {
-		if (playerType == PlayerType.GOALIE) {
-			players.add(new Goalie(firstName, lastName));
-		} else if (playerType == PlayerType.FORWARD) {
-			players.add(new Forward(firstName, lastName));
-		} else {
-			players.add(new Defenceman(firstName, lastName));
-		}
-	}
- 
-public void executePlay(PlayerType playerType) {
-		for (Player player : players) {
-			if (player.getPlayerType() == playerType) {
-				System.out.println(player.play());
-			}
-		}
-	}
- 
-	public ArrayList<Player> getPlayers() {
-		return players;
-	}
- 
-	public String getName() {
-		return teamName;
-	}
+    public void addTeamMember(String firstName, String lastName, PlayerType playerType) {
+        Player newPlayer;
+
+        if (playerType == PlayerType.GOALIE) {
+            newPlayer = new Goalie(firstName, lastName);
+        } else if (playerType == PlayerType.FORWARD) {
+            newPlayer = new Forward(firstName, lastName);
+        } else {
+            newPlayer = new Defenceman(firstName, lastName);
+        }
+
+        players.add(newPlayer);
+    }
+
+    public void executePlay(PlayerType playerType) {
+        for (Player player : players) {
+            if (player.getPlayerType() == playerType) {
+                System.out.println(player.play());
+            }
+        }
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public String getName() {
+        return teamName;
+    }
 }
