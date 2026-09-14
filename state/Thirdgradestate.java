660aee811fe16c5492ff8734b7c12e6863bbba99
shmoney2 <geddsahil@gmail.com>
2026-09-13 23:06:30 -0400

    Finished Thirdgradestate

---
+++ state/Thirdgradestate.java
@@ -0,0 +1,19 @@
+Thirdgradestate · JAVA
+package state;
+ 
+public class ThirdGradeState extends State {
+ 
+    public ThirdGradeState(VocabularyList vocabularyList) {
+        super(vocabularyList);
+        this.words = FileReader.getWords("state/txt/third.txt");
+    }
+ 
+    public void increaseGrade() {
+        System.out.println("You are already in third grade.");
+    }
+ 
+    public void decreaseGrade() {
+        System.out.println("Moving back to second grade.");
+        vocabularyList.setState(vocabularyList.getSecondGradeState());
+    }
+}
