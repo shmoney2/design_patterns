971fbe0ebb3a97b82a45a981504e200f82350cb2
shmoney2 <geddsahil@gmail.com>
2026-09-13 22:49:50 -0400

    Finished vocabularlyList java class

---
+++ state/Vocabularylist.java
@@ -0,0 +1,47 @@
+package state;
+
+public class Vocabularylist {
+    private State state;
+    private FirstGradeState firstGradeState;
+    private SecondGradeState secondGradeState;
+    private ThirdGradeState thirdGradeState;
+ 
+    public VocabularyList() {
+        firstGradeState = new FirstGradeState(this);
+        secondGradeState = new SecondGradeState(this);
+        thirdGradeState = new ThirdGradeState(this);
+        state = firstGradeState;
+    }
+ 
+    public String getNextDefinition() {
+        return state.getNextDefinition();
+    }
+ 
+    public String getMatchingWord(String definition) {
+        return state.getMatchingWord(definition);
+    }
+ 
+    public void increaseGrade() {
+        state.increaseGrade();
+    }
+ 
+    public void decreaseGrade() {
+        state.decreaseGrade();
+    }
+ 
+    public State getFirstGradeState() {
+        return firstGradeState;
+    }
+ 
+    public State getSecondGradeState() {
+        return secondGradeState;
+    }
+ 
+    public State getThirdGradeState() {
+        return thirdGradeState;
+    }
+ 
+    public void setState(State state) {
+        this.state = state;
+    }
+}
