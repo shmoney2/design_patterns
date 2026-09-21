41bc74b1c8fc5fda61198cda08ab812fb53ac3ba
shmoney2 <geddsahil@gmail.com>
2026-09-20 22:55:39 -0400

    finished colorsk

---
+++ singleton/Colors.java
@@ -0,0 +1,10 @@
+
+/**
+ * @author Sahil
+ * All this class contains are the colors for the interactive part
+ */
+public class Colors {
+    public static final String RESET = "\u001B[0m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String MAGENTA = "\u001B[35m";
+}
